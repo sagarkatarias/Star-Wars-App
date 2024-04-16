72afc78c0ec2bc1ac24c9676a2823b248da9c788
sagarkatarias <sagarkataria45@gmail.com>
2024-04-17 00:57:40 +0200

    App bar

---
+++ src/components/CustomAppBar.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom';
+
+const CustomAppBar = () => {
+  const handleMenuOpen = (event:  React.MouseEvent<HTMLButtonElement>) => {
+    // Open the menu
+  };
+
+  return (
+    <AppBar >
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleMenuOpen}>
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h1" style={{ flexGrow: 1 }}>
+          <Link to="/" >
+            Star Wars App
+          </Link>
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default CustomAppBar;
