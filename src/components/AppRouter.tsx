71e8ade75d07dc4bbaeaac29f15efceaa18d8aac
sagarkatarias <sagarkataria45@gmail.com>
2024-04-17 00:57:53 +0200

    App router

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,12 +1,22 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "../routes/Home";
-
+import Characters from "../routes/Characters";
+import FilmTimeline from "../routes/FilmTimeline";
+import { Box } from "@mui/material";
+import NotFound from "../routes/NotFound";
+import Planets from "../routes/Planets";
+import Starships from "../routes/Starships";
 
 export const AppRouter = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <Box >
+      <Routes>
+        <Route path="/" element={<FilmTimeline />} />
+        <Route path="/characters/:id/:name" element={<Characters />} />
+        <Route path="/planets/:id/:name" element={<Planets />} />
+        <Route path="/starships/:id/:name" element={<Starships />} />
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
+    </Box>
   );
 };
 
