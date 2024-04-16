0e9e99fe355406bc521683449bcfe5aec012dbed
sagarkatarias <sagarkataria45@gmail.com>
2024-04-17 00:54:25 +0200

    Remove timeout

--- src/api/Axios.ts
+++ src/api/Axios.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 // Create an axios instance to fetch from swapi.dev
 const apiInstance = axios.create({
   baseURL: `https://swapi.${import.meta.env.VITE_ENV}/api/`,
-  timeout: 1000,
 });
 
 // Handle errors
